709b9f610621c7cde7339decf557dd37547f165f
Vindhya Kankurthi <vindyaj07@gmail.com>
2023-09-06 19:48:25 -0700

    ..

---
+++ WebDev/2-Javascript/test.js
@@ -0,0 +1,9 @@
+// Declare the variables
+let message = "The current count is: ";
+let count = 10;
+
+// Use the addition assignment operator
+message += count;
+
+// Print the value of message
+console.log("Message: ", message);
