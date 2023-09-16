7914eec7cfd13beb926bc14e928b52f4a112ee99
Vindhya Kankurthi <vindyaj07@gmail.com>
2023-09-15 19:51:00 -0700

    commit

---
+++ WebDev/2-Javascript/JS/library.js
@@ -0,0 +1,97 @@
+class Books {
+    constructor(title, author, available = true){
+        this.title = title;
+        this.author = author;
+        this.available = available;
+        
+    }
+}
+
+class Library {
+    constructor(){
+        this.books = [];
+    }
+    addBook(book){
+        if(book instanceof Books){
+            this.books.push(book);
+            console.log(`${book.title} has been  added to the library`);
+        }
+        else {
+            console.log('Invalid Book object. Please enter correct details.');
+        }
+    }
+    checkOutBook(title){
+        try{
+            let found = false;
+            for(let book of this.books){
+                if(book.title === title && book.available){
+                    found = true;
+                    bookTitle.available = false;
+                    console.log(`Checked out : ${book.title}`);
+                    break;
+                }
+            }
+            if(!found){
+                throw new Error(`The book: "${title}" was not found or already checked out`);
+            }
+        }
+        catch(error){
+            console.log(error.message);
+        }
+        
+    }
+    getAvailableBooks(){
+        let bookList = [];
+        for(let book of this.books){
+            if(book.available){
+                bookList.push(book.title);
+            }
+        }
+        console.log(`There are ${bookList.length} titles currently on the shelf: ${bookList.join(", ")}`);
+    }
+    
+}
+
+const newBooks = 
+`[
+    {"title": "JavaScript: The Definitive Guide", "author": "David Flanagan"},
+    {"title": "Head First JavaScript Programming", "author": "Eric T Morrison"},
+    {"title": "Eloquent JavaScript, 3rd Edition", "author": "Marijn Haverbeke"},
+    {"title": "The Principles of Object-Oriented JavaScript", "author": "Nicholas C. Zakas"}
+]`;
+
+ receiveBooks = function(newBooks) {
+    console.log(`Adding new books in our library!`);
+
+    const booksToAdd = JSON.parse(newBooks);
+    for(let book of booksToAdd){
+        libraryObject.addBook(book.title, book.author);
+    }
+}
+
+
+const libraryObject = new Library();
+
+const book1 = new Books("Here, There and Everywhere", "Sudha Murty");
+const book2 = new Books("Wings of Fire", "APJ Abdul Kalam");
+const book3 = new Books("The day that changed Everything", "Catherine Miller");
+const book4 = new Books("One night at the call center", "Chetan Bhagat");
+
+libraryObject.addBook(book1);
+libraryObject.addBook(book2);
+libraryObject.addBook(book3);
+libraryObject.addBook(book4);
+
+libraryObject.checkOutBook("Here, There and Everywhere");
+libraryObject.getAvailableBooks();
+receiveBooks(newBooks);
+
+
+
+
+/*console.log(`There are currently ${library.books.length} books in the library's database.`);
+libraryObject.addBook("Eloquent JavaScript", "Marijn Haverbeke");
+receiveBooks(newBooks);
+libraryObject.checkOutBook("Eloquent JavaScript");
+libraryObject.checkOutBook("Grokking the Coding Interview");
+libraryObject.getAvailableBooks(); */
