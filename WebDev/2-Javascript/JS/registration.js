ed5d4bc65f3cdead0b938010c3c01310022b657c
Vindhya Kankurthi <vindyaj07@gmail.com>
2023-09-16 12:05:41 -0700

    registration

---
+++ WebDev/2-Javascript/JS/registration.js
@@ -0,0 +1,104 @@
+class Student {
+    constructor(name, email){
+        this.name = name;
+        this.email = email;
+    }
+}
+
+class Bootcamp {
+    constructor(name, level, students = []){
+        this.name = name;
+        this.level = level;
+        this.students = students;
+    }
+    registerStudent(studentToRegister){
+        if(studentToRegister instanceof Student){
+            if(!studentToRegister.name || !studentToRegister.email){
+                console.log("Invalid name or email");
+            return false;
+            }
+
+            //console.log(studentToRegister.name);
+            for(let i = 0; i< this.students.length; i++){
+                if(studentToRegister.email === this.students[i].email){
+                    console.log("Email already registered");
+                    return false;
+                }
+                
+            }
+            this.students.push(studentToRegister);
+                    console.log(`${studentToRegister.name} Registered Successfully! in the ${reactBootcamp.name}`);
+                    return true;
+        }
+
+    }
+
+    listStudents(){
+        if(this.students.length == 0){
+            console.log(`No students are registered to the ${this.name} bootcamp`);
+            return false;
+        }
+        console.log(`The students registered in ${this.name} bootcamp are: `);
+        for(let studentList of this.students){
+            console.log(`name : ${studentList.name} email : ${studentList.email}`);
+        }
+        return true;
+    }
+
+    getInfo() {
+        console.log( `The bootcamp's name: ${this.name} and level: ${this.level}`);
+    }
+
+    removeStudent(email){
+        if(this.students instanceof Bootcamp){
+            for(let i = 0; i< this.students.length; i++){
+                if(this.students[i].email === email){
+                    this.students.splice(i, 1);
+                }
+            }
+            console.log(`The student with ${this.email} has been removed`);
+        }
+    }
+}
+
+
+
+testStudent = new Student('Bugs Bunny', 'bugs@bunny.com');
+console.log(testStudent);
+if ( testStudent.name === 'Bugs Bunny' && testStudent.email === 'bugs@bunny.com') {
+    console.log('TASK 1: PASS');
+}
+
+reactBootcamp = new Bootcamp("React", "Advanced");
+console.log(reactBootcamp);
+if ( reactBootcamp.name === 'React' && reactBootcamp.level === 'Advanced'
+        && Array.isArray(reactBootcamp.students) && reactBootcamp.students.length === 0) {
+    console.log('TASK 2: PASS');
+}
+
+const runTest = (bootcamp, student) => {
+    const attemptOne = bootcamp.registerStudent(student);
+    const attemptTwo = bootcamp.registerStudent(student);
+    const attemptThree = bootcamp.registerStudent(new Student("Babs Bunny"));
+    console.log(attemptOne);
+    console.log(attemptTwo);
+    console.log(attemptThree);
+    if(attemptOne && !attemptTwo && !attemptThree){
+        console.log("TASK 3 : PASS");
+    }
+    bootcamp.registerStudent(new Student('Babs Bunny', 'babs@bunny.com'));
+    if(bootcamp.listStudents()){
+        console.log("TASK 4 : PASS 1/2");
+    }
+    bootcamp.students = [];
+    if(!bootcamp.listStudents()){
+        console.log("TASK 4 : PASS 2/2");
+    }
+
+    bootcamp.getInfo();
+    bootcamp.removeStudent("babs@bunny.com");
+
+};
+
+runTest(reactBootcamp, testStudent);
+
